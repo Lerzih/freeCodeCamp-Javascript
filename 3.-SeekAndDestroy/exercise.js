6f984092c393fbe08ddcf710ec7313004a451a67
Lerzih <lerzihbarriales@gmail.com>
2023-10-31 15:55:35 +0000

    Solution to exercise 3

--- 3.-SeekAndDestroy/exercise.js
+++ 3.-SeekAndDestroy/exercise.js
@@ -1,5 +1,16 @@
 function destroyer(arr) {
-  return arr;
+  let parameters = Array.from(arguments);
+  // parameters.pop();
+  parameters.forEach((value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (value === arr[i]) arr[i] = null;
+    }
+  });
+  let result = [];
+  arr.forEach((value) => {
+    if (value !== null) result.push(value);
+  });
+  return result;
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
