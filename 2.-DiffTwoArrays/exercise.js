bb3e88fea095845790a958e17d9c22486dcfcaee
Lerzih <lerzihbarriales@gmail.com>
2023-10-31 10:03:57 +0000

    Solution to exercise 2

--- 2.-DiffTwoArrays/exercise.js
+++ 2.-DiffTwoArrays/exercise.js
@@ -1,6 +1,29 @@
 function diffArray(arr1, arr2) {
-    const newArr = [];
-    return newArr;
-  }
-  
-  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+  let newArr = [];
+
+  newArr = getDiffArray(arr1, arr2);
+  newArr = newArr.concat(getDiffArray(arr2, arr1));
+  return newArr;
+}
+
+function getDiffArray(arr1, arr2) {
+  let resultArray = [];
+    arr1.forEach((element) => {
+    let found = false;
+    let i = 0;
+    do {
+      if (element == arr2[i]) found = true;
+      i++;
+    } while (!found && i < arr2.length);
+    if (found === false) resultArray.push(element);
+  });
+
+  return resultArray;
+}
+
+console.log(
+  diffArray(
+    ["andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+  )
+);
