7e8345dfd924bbecc3fa0abce35e35c109eacee9
Lerzih <lerzihbarriales@gmail.com>
2023-10-31 17:16:15 +0000

    Solution to exercise 4

--- 4.-WhereforeAreThou/exercise.js
+++ 4.-WhereforeAreThou/exercise.js
@@ -1,5 +1,54 @@
 function whatIsInAName(collection, source) {
-
+  let results = [];
+  collection.forEach((value) => {
+    if (isValidCollection(source, value)) results.push(value);
+  });
+  return results;
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+function isValidCollection(validation, value) {
+  let resultsValidation = [];
+  let validationKeys = Object.keys(validation);
+  validationKeys.forEach((key) => {
+    if (value.hasOwnProperty(key)) {
+      resultsValidation.push(true);
+    } else {
+      resultsValidation.push(false);
+    }
+  });
+
+  if (resultsValidation.filter((isValid) => isValid == false).length == 0) {
+    resultsValidation = [];
+    validationKeys.forEach((key) => {
+      if (validation[key] === value[key]) resultsValidation.push(true);
+      else resultsValidation.push(false);
+    });
+  }
+
+  let isValid = resultsValidation.filter((isValid) => isValid == false).length == 0 ?? true | false;
+  return isValid;
+}
+
+console.log(
+  whatIsInAName(
+    [
+      { first: "Romeo", last: "Montague" },
+      { first: "Mercutio", last: null },
+      { first: "Tybalt", last: "Capulet" },
+    ],
+    { last: "Capulet" }
+  )
+);
+console.log(
+  whatIsInAName(
+    [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+    { apple: 1, bat: 2 }
+  )
+);
+
+console.log(
+  whatIsInAName(
+    [{ apple: 1, bat: 2 }, { apple: 1 }, { apple: 1, bat: 2, cookie: 2 }],
+    { apple: 1, cookie: 2 }
+  )
+);
